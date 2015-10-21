a2e8020ff03ed9ec1edac83efb2c049bc7aa93d9
Whitney Murray <wnksmith92@yahoo.com>
2015-10-21 05:19:46 -0500

    button works

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+var $button = $('#loadingButton');
+$button.html('click');
+$button.on('click',loading);
+
+function loading(e){
+	$button.html('loading...');
+	$button.css('color', 'grey');
+	setTimeOut(changeText,4000);
+
+function changeText(){
+	$button.html('submit');
+	$button.css('color', 'green');
+	}
+}
+
+
